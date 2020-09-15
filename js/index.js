6614ef9d293c177bbadf7cca5322f479f3958140
kodecheff <akunneemeka125@gmail.com>
2020-09-16 00:37:46 +0100

    init

---
+++ js/index.js
@@ -0,0 +1,10 @@
+ScrollReveal().reveal('.slider', {delay: 500})
+ScrollReveal().reveal('.slider h1', {delay: 1000})
+ScrollReveal().reveal('.slider p', {delay: 1500})
+ScrollReveal().reveal('.service', {
+  delay: 300,
+  duration: 700,
+  reset: true
+})
+ScrollReveal().reveal('.card', {delay: 500})
+ScrollReveal().reveal('.about', {delay: 500})
